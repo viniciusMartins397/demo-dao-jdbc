e4d6617d0683698c8c26a770f1b0ee85a847b684
Vinicius Martins <leonardoandre867@gmail.com>
2026-06-10 20:30:46 -0300

    SellerDaoJDBC and DaoFactory

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -5,12 +5,10 @@ import java.util.List;
 import model.entities.Seller;
 
 public interface SellerDao {
-	public interface SellerDao {
-		 
 		void insert (Seller obj);
 		void update(Seller obj);
 		void deleteById(Integer id);
 		Seller findById(Integer id);
 		List<Seller>findAll();
 	}
-}
+
